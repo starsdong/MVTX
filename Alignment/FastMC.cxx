56c63993d88173dc92791f309a5f8df1c9070686
starsdong <XDong@lbl.gov>
2022-02-09 16:15:37 -0800

    Random cosmic tracks shooting downwards

--- Alignment/FastMC.cxx
+++ Alignment/FastMC.cxx
@@ -102,82 +102,103 @@ int main(int argc, char **argv)
   mTree->Branch("yG_rc",mT.yG_rc,"yG_rc[nh]/F");
   mTree->Branch("zG_rc",mT.zG_rc,"zG_rc[nh]/F");
 
+  TH2F *hMomEtaPhi = new TH2F("MomEtaPhi","",200,0,TMath::Pi(),200,-1.5,1.5);
+
   std::cout << " Generate Initial Cosmic tracks .... " << std::endl;  
   //////////////////////////////////////////////////  
   // Generating the cosmic ray track
-  //////////////////////////////////////////////////  
-  const TVector3 origin(0., 10., 0.);
-  const Double_t thetaP = 0.;  // angle w.r.t Y-axis going down wards - cosmic ray
-  const Double_t momP = 3.0; // 3.0 GeV
-  Double_t pyP = - momP * TMath::Cos(thetaP);
-
+  //////////////////////////////////////////////////
+  const Double_t r_origin = 5.;
+  const Double_t z_origin_Max = 15;  // +/- 15 cm window
   TRandom3 *gRandom = new TRandom3();
-  Double_t phi_xz = gRandom->Rndm()*TMath::Pi()*2.;  // phi in x-z plane
-  Double_t pxP = momP * TMath::Sin(thetaP) * TMath::Cos(phi_xz);
-  Double_t pzP = momP * TMath::Sin(thetaP) * TMath::Sin(phi_xz);
-  TVector3 pmom(pxP, pyP, pzP);
 
-  StPhysicalHelix cosmicRay(pmom, origin, 0, 0);
-  //////////////////////////////////////////////////  
-  std::cout << " Calculating projections to stave planes " << std::endl;
-
-  mT.ox = origin.x();
-  mT.oy = origin.y();
-  mT.oz = origin.z();
-  mT.px = pmom.x();
-  mT.py = pmom.y();
-  mT.pz = pmom.z();
-  
-  int nh = 0;
-  for(int i=NL-1;i>=0;i--) {  // going from most outer layer
-    for(int j=0;j<NS[i];j++) {
-      double *tra = stave[i][j]->GetTranslation();
-      double *rot = stave[i][j]->GetRotationMatrix();
-
-      TVector3 ori(tra[0], tra[1], tra[3]);
-      TVector3 norm(rot[1], rot[4], rot[7]); // normal direction of the plane
-      double s = cosmicRay.pathLength(ori, norm);
-      if(s<0) continue;  // cannot go backwards
-      double hitG_mc[3] = {cosmicRay.x(s), cosmicRay.y(s), cosmicRay.z(s)};
-      double hitL_mc[3] = {-999., -999., -999.};
-      stave[i][j]->MasterToLocal(hitG_mc, hitL_mc);
-      TVector3 hit_mc(hitL_mc[0], hitL_mc[1], hitL_mc[2]);
-      
-      if(!onStave(hit_mc)) continue;
-
-      std::cout << "MC Hit on stave:   layer# " << i << " sector# " << j << std::endl;
-      std::cout << "     xyz = " << hitL_mc[0] << " " << hitL_mc[1] << " " << hitL_mc[2] << std::endl;
-
-      double hitL_rc[3];;
-      hitL_rc[0] = gRandom->Gaus(hitL_mc[0], PitchSize/sqrt(12.));
-      hitL_rc[1] = 0.;
-      hitL_rc[2] = gRandom->Gaus(hitL_mc[2], PitchSize/sqrt(12.));
-      double hitG_rc[3] = {-999., -999., -999.};
-      stave[i][j]->LocalToMaster(hitL_rc, hitG_rc);
-
-      mT.id[nh] = i * 100 + j;
-      mT.xL_mc[nh] = hitL_mc[0];
-      mT.yL_mc[nh] = hitL_mc[1];
-      mT.zL_mc[nh] = hitL_mc[2];
-      mT.xG_mc[nh] = hitG_mc[0];
-      mT.yG_mc[nh] = hitG_mc[1];
-      mT.zG_mc[nh] = hitG_mc[2];
-      mT.xL_rc[nh] = hitL_rc[0];
-      mT.yL_rc[nh] = hitL_rc[1];
-      mT.zL_rc[nh] = hitL_rc[2];
-      mT.xG_rc[nh] = hitG_rc[0];
-      mT.yG_rc[nh] = hitG_rc[1];
-      mT.zG_rc[nh] = hitG_rc[2];
-
-      nh++;
+  for(int ievt = 0; ievt < Nevt; ievt++) {
+    // starting point  
+    Double_t phi_origin = gRandom->Rndm()*TMath::Pi(); // upper hemisphere
+    TVector3 origin(r_origin*TMath::Cos(phi_origin), r_origin*TMath::Sin(phi_origin), gRandom->Rndm()*z_origin_Max*2. - z_origin_Max);
+    
+    // starting momentum direction
+    const Double_t momP = 3.0; // 3.0 GeV
+    Double_t thetaP = gRandom->Rndm()*TMath::Pi()/2.0;   // 0 - Pi/2   go downwards - cosmic ray  
+    Double_t pyP = - momP * TMath::Cos(thetaP);
+    Double_t phi_xz = gRandom->Rndm()*TMath::Pi()*2.;  // phi in x-z plane
+    Double_t pxP = momP * TMath::Sin(thetaP) * TMath::Cos(phi_xz);
+    Double_t pzP = momP * TMath::Sin(thetaP) * TMath::Sin(phi_xz);
+    TVector3 pmom(pxP, pyP, pzP);
+    if(ievt%1000==0) {
+      std::cout << " === Processing event # " << ievt << std::endl;
+      std::cout << "    origin point " << origin.x() << " " << origin.y() << " " << origin.z() << std::endl;
+      std::cout << "    momentum dir " << pmom.x() << " " << pmom.y() << " " << pmom.z() << std::endl;      
     }
-  }
-  std::cout << " # of Hits in this event " << nh << std::endl;
-  mT.nh = nh;
-  mTree->Fill();
+    
+    StPhysicalHelix cosmicRay(pmom, origin, 0, 0);
+    //////////////////////////////////////////////////  
+    //    std::cout << " Calculating projections to stave planes " << std::endl;
+    
+    mT.ox = origin.x();
+    mT.oy = origin.y();
+    mT.oz = origin.z();
+    mT.px = pmom.x();
+    mT.py = pmom.y();
+    mT.pz = pmom.z();
+    
+    int nh = 0;
+    for(int i=NL-1;i>=0;i--) {  // going from most outer layer
+      for(int j=0;j<NS[i];j++) {
+	double *tra = stave[i][j]->GetTranslation();
+	double *rot = stave[i][j]->GetRotationMatrix();
+	
+	TVector3 ori(tra[0], tra[1], tra[3]);
+	TVector3 norm(rot[1], rot[4], rot[7]); // normal direction of the plane
+	double s = cosmicRay.pathLength(ori, norm);
+	if(s<0) continue;  // cannot go backwards
+	double hitG_mc[3] = {cosmicRay.x(s), cosmicRay.y(s), cosmicRay.z(s)};
+	double hitL_mc[3] = {-999., -999., -999.};
+	stave[i][j]->MasterToLocal(hitG_mc, hitL_mc);
+	TVector3 hit_mc(hitL_mc[0], hitL_mc[1], hitL_mc[2]);
+	
+	if(!onStave(hit_mc)) continue;
 
+	if(ievt%1000==0) {
+	  std::cout << "MC Hit on stave:   layer# " << i << " sector# " << j << std::endl;
+	  std::cout << "     xyz = " << hitL_mc[0] << " " << hitL_mc[1] << " " << hitL_mc[2] << std::endl;
+	}
+	
+	double hitL_rc[3];;
+	hitL_rc[0] = gRandom->Gaus(hitL_mc[0], PitchSize/sqrt(12.));
+	hitL_rc[1] = 0.;
+	hitL_rc[2] = gRandom->Gaus(hitL_mc[2], PitchSize/sqrt(12.));
+	double hitG_rc[3] = {-999., -999., -999.};
+	stave[i][j]->LocalToMaster(hitL_rc, hitG_rc);
+	
+	mT.id[nh] = i * 100 + j;
+	mT.xL_mc[nh] = hitL_mc[0];
+	mT.yL_mc[nh] = hitL_mc[1];
+	mT.zL_mc[nh] = hitL_mc[2];
+	mT.xG_mc[nh] = hitG_mc[0];
+	mT.yG_mc[nh] = hitG_mc[1];
+	mT.zG_mc[nh] = hitG_mc[2];
+	mT.xL_rc[nh] = hitL_rc[0];
+	mT.yL_rc[nh] = hitL_rc[1];
+	mT.zL_rc[nh] = hitL_rc[2];
+	mT.xG_rc[nh] = hitG_rc[0];
+	mT.yG_rc[nh] = hitG_rc[1];
+	mT.zG_rc[nh] = hitG_rc[2];
+	
+	nh++;
+      }  // end loop j - sector
+    }  // end loop i - layer
+    if(ievt%1000==0)
+      std::cout << " # of Hits in this event " << nh << std::endl;
+    mT.nh = nh;
+    if(nh>=1) {
+      mTree->Fill();
+      hMomEtaPhi->Fill(-pmom.Phi(), pmom.Eta());
+    }
+  } // end loop ievt
   
   mTree->Write();
+  hMomEtaPhi->Write();
   f.Close();
 
   return 0;
